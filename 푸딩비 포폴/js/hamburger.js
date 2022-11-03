90f65b48a295fadff3c6b6f92bbb150043bef5fe
푸딩비 <91423376+chamypuppy@users.noreply.github.com>
2022-11-03 17:21:17 +0900

    2022 푸딩비 포폴 디자인 개선 ver
    벚꽃푸딩톡 팝업창 미디어쿼리 424px까지 완료하고 
    빠르게 다음 프로젝트도 일단 올리고자 프로젝트2 JS Challenge를 먼저 작업하고 있다. 현재는 프로젝트2 미디어쿼리 진행 중! 일단 300px대까지 설정하지 않을까 싶다.
    그리고 햄버거 버튼의 X표시 애니메이션이 잠시 먹통이었는데 html 문서에서 클래스 네임을 변경하고 javascript에서는 변경하지 않은 것이 원인이었다 ㅠㅠ
    그래도 잘 해결되어서 다행이다😄!!

--- 푸딩비 포폴/js/hamburger.js
+++ 푸딩비 포폴/js/hamburger.js
@@ -3,6 +3,6 @@ const hamburgerIcon = document.querySelector(".nav__hamburger");
 const sideWrapper = document.querySelector(".side__wrapper");
 
 hamburgerIcon.addEventListener("click", (event) => {
-  event.currentTarget.classList.toggle("active__hamburger")
+  event.currentTarget.classList.toggle("active__close__btn")
   sideWrapper.classList.toggle("active");
 });
